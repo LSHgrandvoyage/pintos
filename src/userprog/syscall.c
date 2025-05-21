bc4e7f7ddba59a3a34681825b6555cda022dd7d7
LSHgrandvoyage <2718sh0517@gm.gist.ac.kr>
2025-05-20 21:23:44 -0700

    CREATE system call is implemented + CREATE test cases all pass

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,11 +1,25 @@
 #include "userprog/syscall.h"
 #include <stdio.h>
 #include <syscall-nr.h>
+#include <string.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "threads/vaddr.h"
+#include "devices/shutdown.h"
+#include "userprog/pagedir.h"
+#include "userprog/process.h"
+#include "filesys/filesys.h"
+
+typedef int pid_t;
 
 static void syscall_handler (struct intr_frame *);
+
+void halt (void);
+void exit (int status);
+pid_t exec (const char *cmd_line);
+int wait (pid_t pid);
+bool create (const char *file, unsigned initial_size);
+int write (int fd, const void *buffer, unsigned size);
 
 void
 syscall_init (void) 
@@ -15,7 +29,7 @@ syscall_init (void)
 
 static void
 check_validation(const void *vaddr){
-  if (!is_user_vaddr(vaddr) || !pagedir_get_page(thread_current()->pagedir, vaddr)){
+  if (vaddr == NULL || !is_user_vaddr(vaddr) || !pagedir_get_page(thread_current()->pagedir, vaddr)){
     exit(-1);
   }
 }
@@ -24,15 +38,28 @@ static void
 syscall_handler (struct intr_frame *f) 
 {
   uint32_t *esp = (uint32_t *)f->esp;
-  
   check_validation(esp);
 
   switch (esp[0]){
-    case SYS_HALT: halt(); break;
-    case SYS_EXIT: check_validation(&esp[1]); exit((int)esp[1]); break;  
-    case SYS_EXEC: break;
-    case SYS_WAIT: break;
-    case SYS_CREATE: break;
+    case SYS_HALT: 
+	halt(); break;
+    case SYS_EXIT: 
+	check_validation(&esp[1]);
+	exit((int)esp[1]); 
+	break;  
+    case SYS_EXEC: 
+        check_validation(&esp[1]);
+        f->eax = exec((const char *)esp[1]);
+        break;
+    case SYS_WAIT:
+        check_validation(&esp[1]);
+	f->eax = wait((pid_t)esp[1]);
+	break;
+    case SYS_CREATE:
+	check_validation((const void *)esp[1]);
+	check_validation(&esp[2]);
+	f->eax = create((const char *)esp[1], (unsigned)esp[2]);
+	break;
     case SYS_REMOVE: break;
     case SYS_OPEN: break;
     case SYS_FILESIZE: break;
@@ -41,7 +68,7 @@ syscall_handler (struct intr_frame *f)
         check_validation(&esp[1]);
         check_validation(&esp[2]);
         check_validation(&esp[3]);
-        f->eax = write((int)esp[1], (void *)esp[2], (unsigned)esp[3]); 
+        f->eax = write((int)esp[1], (void *)esp[2], (unsigned)esp[3]);
         break;
     case SYS_SEEK: break;
     case SYS_TELL: break;
@@ -64,7 +91,27 @@ exit (int status){
   thread_exit();
 }
 
-int write (int fd, const void *buffer, unsigned size){
+pid_t
+exec (const char *cmd_line){
+  check_validation(cmd_line);
+  return process_execute(cmd_line);
+}
+
+int
+wait (pid_t pid){
+  return process_wait(pid);
+}
+
+bool
+create (const char *file, unsigned initial_size){
+  if (file == NULL || strlen(file) == 0){
+    exit(-1);
+  }
+  return filesys_create(file, initial_size);
+}
+
+int
+write (int fd, const void *buffer, unsigned size){
   if (fd == 1){
    putbuf(buffer, size);
    return size;
